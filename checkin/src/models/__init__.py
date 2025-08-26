3b4ff6f2b9a9fc7d8927e3557ee7fb9b8503cdea
Samuel Mauli <101298616+SamuelMauli@users.noreply.github.com>
2025-08-26 11:27:21 -0300

    Delete checkin directory

--- checkin/src/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# Import models to make them available when importing from models package
-from .database import Database
-from .campanha_escalavel import CampanhaEscalavel
-from .van_model import VanModel
-from .checking_model import CheckingModel
